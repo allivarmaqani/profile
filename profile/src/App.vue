a9669416747e6af1a7bebf011fb43876b81049cc
allivarmaqani <allivarmaqani@gmail.com>
2025-04-03 12:18:19 -0700

    make project profile with vue

---
+++ profile/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-[#D4EBF8] dark:bg-[#080326] min-h-screen">
+    <Navbar/>
+    <HeroSection />
+    <Counter />
+    <Services />
+    <Skills />
+    <Whyme />
+  </div>
+</template>
+
+<script setup>
+import { defineAsyncComponent } from 'vue';
+
+const Navbar = defineAsyncComponent(() => import('@/components/layout/Navbar.vue'));
+const HeroSection = defineAsyncComponent(() => import('@/components/layout/HeroSection.vue'));
+const Counter = defineAsyncComponent(() => import('@/components/layout/Counter.vue'));
+const Services = defineAsyncComponent(() => import('@/components/layout/Services.vue')); 
+const Skills = defineAsyncComponent(() => import('@/components/layout/Skills.vue')); 
+const Whyme = defineAsyncComponent(() => import('@/components/layout/Whyme.vue')); 
+
+
+</script>
---
+++ profile/src/components/layout/Whyme.vue
@@ -0,0 +1,12 @@
+<template>
+    <section id="whyme" class="mt-32">
+     <SectionHeader title="why me" />
+     <div class="grid grid-cols-1 lg:grod-cols-2 p-12 overflow-hidden">
+      <Education data-aos="fade-left" />
+     </div>
+    </section>
+</template>
+<script setup>
+import SectionHeader from '../UI/SectionHeader.vue';
+import Education from '../layout/Education.vue';
+</script>
