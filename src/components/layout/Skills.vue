30bf024b07a3bda861ac5db7c901df3233fc92ab
allivarmaqani <allivarmaqani@gmail.com>
2025-04-04 02:04:36 -0700

    html

---
+++ src/components/layout/Skills.vue
@@ -0,0 +1,95 @@
+<template>
+  <section class="mt-32" id="skills">
+   <SectionHeader title="My Skills" />
+   <div class="mt-20 flex justify-center">
+    <ul class="flex flex-wrap justify-center items-center">
+        <li
+        ref="skillRefs"
+         v-for="(element,index) in skills" :key="index"
+        :class="`mx-[15px] rounded-[12px] mb-7 bg-gradient-to-t ${element.bgGradient}`">
+         <div class="rounded-[12px] bg-primary mt-[3px] p-12 md:p-5 text-center">
+            <h3 class="font-bold text-[35px] text-white flex items-center justify-center">%
+                <Countup v-if="visibleItems[index]" :endVal="element.percentage" :startVal="0" :duration="2" />
+            </h3>
+            <p class="font-normal text-[16px]" :style="{color:element.textColor}">{{ element.title }}</p>
+         </div>
+        </li>
+    </ul>
+   </div>
+  </section>
+</template>
+<script setup>
+import { ref,onMounted } from 'vue';
+import SectionHeader from '../UI/SectionHeader.vue';
+
+const skills =ref([
+    {
+        percentage:98,
+        title:'Figma',
+        bgGradient:'to-[#acac39] from-[#1f1e1c99]',
+        textColor:'#FFE600'
+    },
+    {
+        percentage:98,
+        title:'java',
+        bgGradient:'to-[#acac39] from-[#1f1e1c99]',
+        textColor:'#59c378'
+    },
+    {
+        percentage:88,
+        title:'Photoshop',
+        bgGradient:'to-[#dd584f99] from-[#1f1e1c99]',
+        textColor:'#59c378'
+    },
+    {
+        percentage:77,
+        title:'Vue js',
+        bgGradient:'to-[#ff9a0099] from-[#1f1e1c99]',
+        textColor:'#ff0a00'
+    },
+    {
+        percentage:43,
+        title:'wordpress',
+        bgGradient:'to-[#02a9ff99] from-[#1f1e1c99]',
+        textColor:'#FFE600'
+    },
+    {
+        percentage:98,
+        title:'React js',
+        bgGradient:'to-[#9e0099] from-[#1f1e1c99]',
+        textColor:'#ad00ff'
+    },
+    {
+        percentage:98,
+        title:'Html',
+        bgGradient:'to-[#acac39] from-[#1f1e1c99]',
+        textColor:'#ffe600'
+    },
+    {
+        percentage:98,
+        title:'javascript',
+        bgGradient:'to-[#59c37899] from-[#1f1e1c99]',
+        textColor:'#59c378'
+    },
+]);
+
+
+const visibleItems = ref(skills.value.map(()=>{false}));
+const skillRefs = ref([]);
+onMounted(()=>{
+    const observer = new IntersectionObserver(
+        (entries)=>{
+            entries.forEach((entry)=>{
+                if(entry.isIntersecting){
+                    const index=skillRefs.value.indexOf(entry.target);
+                    if(index !== -1){
+                        visibleItems.value[index] = true;
+                    }
+                }
+            })
+        },
+        {threshold:0.3}
+    );
+    skillRefs.value.forEach((el)=>{observer.observe(el)});
+})
+</script>
